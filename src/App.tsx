4f818878d52abfadf226c58befc345dc05c55877
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-19 15:08:27 +0100

    Boot playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import './App.css';
+import {useEffect} from "react";
+import {startPlaygroundWeb} from "@wp-playground/client";
+
 export function App() {
-  return (
-    <>
-      hello
-    </>
-  )
+    useEffect(() => {
+        initPlayground().catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <iframe
+            title="playground"
+            id="playground"
+            src="https://playground.wordpress.net"
+        />
+    );
+}
+
+async function initPlayground() {
+    const iframe = document.getElementById('playground') as HTMLIFrameElement;
+    if (iframe.src !== '') {
+        return;
+    }
+    const client = await startPlaygroundWeb({
+        iframe,
+        remoteUrl: 'https://playground.wordpress.net/remote.html',
+    });
+    await client.isReady;
 }
