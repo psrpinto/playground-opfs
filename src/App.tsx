ecdfed4b5c7bf0cac84620fb3874fbbd6441f068
Paulo Pinto <paulo.pinto@automattic.com>
2024-09-19 16:33:02 +0100

    useBootPlayground()

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
 import './App.css';
-import {useEffect} from "react";
-import {startPlaygroundWeb} from "@wp-playground/client";
+import {useBootPlayground} from './useBootPlayground';
 
 export function App() {
-    useEffect(() => {
-        initPlayground().catch((err) => console.log(err));
-    }, []);
+    const { playground, iframeRef } = useBootPlayground();
 
     return (
         <iframe
             title="playground"
             id="playground"
-            src="https://playground.wordpress.net"
+            ref={iframeRef}
         />
     );
-}
-
-async function initPlayground() {
-    const iframe = document.getElementById('playground') as HTMLIFrameElement;
-    if (iframe.src !== '') {
-        return;
-    }
-    const client = await startPlaygroundWeb({
-        iframe,
-        remoteUrl: 'https://playground.wordpress.net/remote.html',
-    });
-    await client.isReady;
 }
---
+++ src/useBootPlayground.ts
@@ -0,0 +1,44 @@
+import {useEffect, useRef, useState} from "react";
+import {PlaygroundClient, startPlaygroundWeb} from "@wp-playground/client";
+
+const remoteUrl = 'https://playground.wordpress.net/remote.html';
+
+export function useBootPlayground() {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+    const iframe = iframeRef.current;
+    const [awaitedIframe, setAwaitedIframe] = useState(false);
+    const [playground, setPlayground] = useState<PlaygroundClient>();
+
+    useEffect(() => {
+        if (!iframe) {
+            // Iframe ref is likely not set on the initial render.
+            // Re-render the current component to start the playground.
+            if (!awaitedIframe) {
+                setAwaitedIframe(true);
+            }
+            return;
+        }
+
+        async function boot() {
+            let playgroundTmp: PlaygroundClient | undefined = undefined;
+            try {
+                await startPlaygroundWeb({
+                    iframe: iframe!,
+                    remoteUrl,
+                    onClientConnected: (playground) => {
+                        playgroundTmp = playground;
+                    },
+                });
+            } finally {
+                if (playgroundTmp) {
+                    setPlayground(() => playgroundTmp);
+                }
+            }
+
+
+        }
+        boot().catch((err) => console.error(err));
+    }, [iframe, awaitedIframe]);
+
+    return { playground, iframeRef };
+}
